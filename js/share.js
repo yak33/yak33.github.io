cda5e0b3a8ac0d4dc5b1b0f566afad52a9feb4d9
zhangchao <zhangchao@yorma.net>
2020-03-06 14:57:53 +0800

    Site updated: 2020-03-06 14:57:50

--- js/share.js
+++ js/share.js
@@ -1,114 +1,75 @@
-(() => {
-  function stringfy(params = {}) {
-    let str = '?';
-    for(let key of Reflect.ownKeys(params)) {
-      let value = !!params[key] ? encodeURIComponent(params[key]) : ''; 
-      str = `${str}${key}=${value}&`;
+
+function generate(url, opts) {
+  var url = url.replace(/<%-sUrl%>/g, encodeURIComponent(opts.sUrl))
+    .replace(/<%-sTitle%>/g, opts.sTitle)
+    .replace(/<%-sDesc%>/g, opts.sDesc)
+    .replace(/<%-sPic%>/g, encodeURIComponent(opts.sPic));
+
+  window.open(url);
+}
+
+function showWX() {
+  let $wx = $('.wx-share-modal')
+  let $mask = $('#share-mask')
+  $wx.addClass('in')
+  $wx.addClass('ready')
+  $mask.show()
+}
+
+function hideWX() {
+  let $wx = $('.wx-share-modal')
+  let $mask = $('#share-mask')
+  $wx.removeClass('in')
+  $wx.removeClass('ready')
+  $mask.hide()
+}
+
+function handleClick(type, opts) {
+  if (type === 'weibo') {
+    generate('http://service.weibo.com/share/share.php?url=<%-sUrl%>&title=<%-sTitle%>&pic=<%-sPic%>', opts)
+  } else if (type === 'qq') {
+    generate('http://connect.qq.com/widget/shareqq/index.html?url=<%-sUrl%>&title=<%-sTitle%>&source=<%-sDesc%>', opts)
+  } else if (type === 'douban') {
+    generate('https://www.douban.com/share/service?image=<%-sPic%>&href=<%-sUrl%>&name=<%-sTitle%>&text=<%-sDesc%>', opts)
+  } else if (type === 'qzone') {
+    generate('http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=<%-sUrl%>&title=<%-sTitle%>&pics=<%-sPic%>&summary=<%-sDesc%>', opts)
+  } else if (type === 'facebook') {
+    generate('https://www.facebook.com/sharer/sharer.php?u=<%-sUrl%>', opts)
+  } else if (type === 'twitter') {
+    generate('https://twitter.com/intent/tweet?text=<%-sTitle%>&url=<%-sUrl%>&via=<%-config.url%>', opts)
+  } else if (type === 'google') {
+    generate('https://plus.google.com/share?url=<%-sUrl%>', opts)
+  } else if (type === 'weixin') {
+    showWX();
+  }
+}
+
+let init = function () {
+  let $sns = document.querySelectorAll('.share-sns');
+  if (!$sns || $sns.length === 0) return;
+
+  let sUrl = window.location.href;
+  let sTitle = document.querySelector('title').innerHTML;
+  let $img = document.querySelectorAll('.article-entry img');
+  let sPic = $img.length ? document.querySelector('.article-entry img').getAttribute('src') : '';
+  if ((sPic !== '') && !/^(http:|https:)?\/\//.test(sPic)) {
+    sPic = window.location.origin + sPic
+  }
+
+  $sns.forEach(($em) => {
+    $em.onclick = (e) => {
+      let type = $em.getAttribute('data-type')
+      handleClick(type, {
+        sUrl: sUrl,
+        sPic: sPic,
+        sTitle: sTitle,
+        sDesc: sTitle
+      })
     }
-    return str.slice(0, str.length - 1);
-  }
+  })
 
-  function toggleShareBtn() {
-    let show = false;
-    const shareBtnDOM = document.querySelector('#share-btn');
+  document.querySelector('#mask').onclick = hideWX
+  document.querySelector('.modal-close').onclick = hideWX
+}
 
-    return (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-      show = !show;
-      shareBtnDOM.style.display = show ? 'flex' : 'none';
-    };
-  }
-
-  const mapSocialToUrl = (() => {
-    const baseUrls = {
-      twitter: 'https://twitter.com/intent/tweet',
-      facebook: 'https://www.facebook.com/sharer/sharer.php',
-      qq: 'http://connect.qq.com/widget/shareqq/index.html',
-      weibo: 'http://service.weibo.com/share/share.php'
-    };
-
-    const title = document.title;
-    const description = document.querySelector("meta[name='description']").getAttribute('content');
-    const url = `${window.location.origin}${window.location.pathname}`;
-
-    const params = {
-      twitter: {
-        url,
-        text: `${title}\n\n${description}\n\n`,
-        via: window.location.origin
-      },
-      facebook: {
-        u: url
-      },
-      weibo: {
-        url,
-        title: `${title}\n\n${description}`
-      },
-      qq: {
-        url,
-        title,
-        desc: description
-      },
-    };
-
-    return {
-      twitter: `${baseUrls.twitter}${stringfy(params.twitter)}`,
-      facebook: `${baseUrls.facebook}${stringfy(params.facebook)}`,
-      weibo: `${baseUrls.weibo}${stringfy(params.weibo)}`,
-      qq: `${baseUrls.qq}${stringfy(params.qq)}`,
-    }
-  })();
-
-  const pfxCls = '#share-btn';
-  const { share } = window.AD_CONFIG;
-  const socials = Reflect.ownKeys(share).filter(social => share[social]);
-
-  for(let social of socials) {
-    if(social === 'wechat') {
-      continue;
-    }
-    document
-      .querySelector(`${pfxCls}-${social}`)
-      .setAttribute('href', mapSocialToUrl[social]);
-  }
-
-  if(!socials.includes('wechat')) {
-    return;
-  }
-
-  // wechat share by qrcode
-  document.querySelector('#share-btn-wechat').addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    const layer = document.querySelector('#site-layer'),
-      container = document.querySelector('#site-layer-container'),
-      title = document.querySelector('#site-layer-title'),
-      newDOM = document.createElement('div');
-
-    layer.style.display = 'block';
-    title.innerHTML = '微信分享';
-    container.appendChild(newDOM);
-
-    const qrcode = new QRCode(newDOM, {
-      text: `${window.location.origin}${window.location.pathname}`,
-      width: 256,
-      height: 256,
-      colorDark: "#000000",
-      colorLight: "#ffffff",
-      correctLevel : QRCode.CorrectLevel.H
-    });
-
-    window.AD_CONFIG.layer.add(() => {
-      title.innerHTML = '';
-      qrcode.clear();
-      newDOM.remove();
-    });
-  });
-
-  // control btn panel if show in mobile phone
-  if(socials.length > 0) {
-    document.querySelector('#site-toggle-share-btn').addEventListener('click', toggleShareBtn());
-  }
-})();
+init()
